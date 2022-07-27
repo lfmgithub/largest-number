358b59f6056fb042f922b50374f646c426168a42
lfmgithub <vaccari.lf@gmail.com>
2022-07-27 10:15:47 +0100

    Add unit test.

--- src/test/java/com/lf/largestnumber/ApplicationTest.java
+++ src/test/java/com/lf/largestnumber/ApplicationTest.java
@@ -1,4 +1,22 @@
 package com.lf.largestnumber;
 
+import com.lf.largestnumber.number.NumberImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ApplicationTest {
+
+    private NumberImpl numberImpl;
+
+    @Before
+    public void setup() {
+        numberImpl = new NumberImpl(new String[] {"190","45","89","9","989","9","0","-50"});
+    }
+
+    @Test
+    public void findLargestPositiveNumberPossible() {
+        assertEquals("9998989451900", numberImpl.findLargestNumber());
+    }
 }
